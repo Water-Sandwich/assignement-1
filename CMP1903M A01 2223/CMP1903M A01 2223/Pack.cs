137ecdc3ae1c265b101d369cac2fdfa8cb92766f
D Vilimavicius <deivoro23@gmail.com>
2023-03-05 15:18:11 +0000

    beginning

---
+++ CMP1903M A01 2223/CMP1903M A01 2223/Pack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Pack
+    {
+        List<Card> pack;
+
+        public Pack()
+        {
+            //Initialise the card pack here
+        }
+
+        public static bool shuffleCardPack(int typeOfShuffle)
+        {
+            //Shuffles the pack based on the type of shuffle
+            return false;
+        }
+        public static Card deal()
+        {
+            //Deals one card
+            return new Card();
+        }
+        public static List<Card> dealCard(int amount)
+        {
+            //Deals the number of cards specified by 'amount'
+            return new List<Card>();
+        }
+    }
+}
